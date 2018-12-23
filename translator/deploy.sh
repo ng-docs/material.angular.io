13279810cd31f251cd4110e1e7dd832cee102e73
Zhicheng WANG <zcwang@thoughtworks.com>
2018-12-23 16:09:30 +0800

    fix: 错误语句

--- translator/deploy.sh
+++ translator/deploy.sh
@@ -8,8 +8,8 @@ set -x
 
 npm run prod-build
 
-cp -r ../dist/* ../../material-cn-prebuilt
-cd ../../material-cn-prebuilt
+cp -r dist/* ../material-cn-prebuilt
+cd ../material-cn-prebuilt
 cp index.html 404.html
 git add .
 git commit -am "publish"
