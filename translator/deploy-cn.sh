a5c00233a9b598868146fd8eb0936eb55bc4ec88
Zhicheng WANG <zcwang@thoughtworks.com>
2019-02-24 17:38:03 +0800

    fix: 自动发布到预览站

---
+++ translator/deploy-cn.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+if [[ ! -d "./material-cn-preview" ]]
+then
+    git clone https://asnowwolf:${GITHUB_ACCESS_TOKEN}@github.com/ng-docs/material-cn-preview.git
+fi
+
+cp -r dist/* ./material-cn-preview
+
+cd ./material-cn-preview
+
+commitSha=$(git rev-parse --short HEAD)
+commitMessage=$(git log --oneline -n 1)
+
+cp index.html 404.html
+
+git add .
+git commit --allow-empty -am "${commitMessage}"
+
+git push
+
+cd -
